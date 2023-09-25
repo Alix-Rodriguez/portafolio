18cda77c816407847ec067c81adead7086845462
Alix Rodriguez <alix.rodriguez@jirehpro.co>
2023-09-24 21:50:31 -0400

    animacione añadidas y botom ws

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { Menu } from '../components/Menu';
 import { Slider } from '../components/Slider';
 import Typewriter from 'typewriter-effect';
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import { useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
 
 const gradiant = {
@@ -14,9 +15,47 @@ const gradiant = {
 
 
 export default function Home() {
+
+  
+  useEffect(() => {
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      
+    
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 100, // values from 0 to 3000, with step 50ms
+      duration: 1500, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: true, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    
+    });
+    
+  }, []);
+
   return (
     <>
-     <FloatingWhatsApp  />
+     <FloatingWhatsApp
+      phoneNumber="+584248428780"
+      avatar="/logo/logo1.png"
+      accountName="Start - Tech"
+      statusMessage="Donde Las Ideas Cobran Vida"
+      chatMessage="Hola un gusto saludarle!!!. Como podemos ayudarle?  "
+      placeholder="Escriba un mensaje..."
+      allowClickAway={true}
+      notification={true}
+      />
+
       <Menu />
       <Slider />
 
@@ -42,21 +81,21 @@ export default function Home() {
           </h1>
         </div>
 
-        <div className='flex flex-col py-5 lg:flex-row gap-y-9 h-4/5 items-center lg:gap-x-4 px-5 sm:px-11 w-full'>
+        <div  className='flex flex-col py-5 lg:flex-row gap-y-9 h-4/5 items-center lg:gap-x-4 px-5 sm:px-11 w-full'>
 
-          <div className='border-4 shadow-2xl h-[90%] rounded-sm borde-inset p-7  flex justify-center flex-col items-center gap-y-10'>
+          <div data-aos="fade-right" className='border-4 shadow-2xl h-[90%] rounded-sm borde-inset p-7  flex justify-center flex-col items-center gap-y-10'>
             <Image src='icon/compu.svg' alt="Computadora" width={70} height={70} ></Image>
             <h2 className='titulo capitalize font-bold text-lg ' >Desarrollo web</h2>
             <h4 className='subtitulo w-11/12 text-center'>Nuestro equipo de trabajo puede desarrollar desde páginas tipo catálogo, las cuales muestran sus productos de una forma sencilla y efectiva, hasta sitios web en varios idiomas, todo de acuerdo con sus necesidades.</h4>
           </div>
 
-          <div className='border-4 shadow-2xl h-[90%] rounded-sm borde-inset p-7  flex justify-center flex-col items-center gap-y-10'>
+          <div  data-aos="fade-down" className='border-4 shadow-2xl h-[90%] rounded-sm borde-inset p-7  flex justify-center flex-col items-center gap-y-10'>
             <Image src='icon/diseño.svg' alt="Diseño" width={70} height={70} ></Image>
             <h2 className='titulo capitalize font-bold text-lg '>Diseño a la medida</h2>
             <h4 className='w-11/12 subtitulo text-center'>¡Descubre el poder de un buen diseño web con nuestro servicio! En nuestro sitio, transformamos tus ideas en realidad digital. Nos especializamos en crear sitios web atractivos, intuitivos y de alto rendimiento que capturan la esencia de tu negocio y conectan con tus clientes.</h4>
           </div>
 
-          <div className='border-4 shadow-2xl h-[90%] rounded-sm borde-inset p-7  flex justify-center flex-col items-center gap-y-10'>
+          <div data-aos="fade-left" className='border-4 shadow-2xl h-[90%] rounded-sm borde-inset p-7  flex justify-center flex-col items-center gap-y-10'>
             <Image src='icon/google.svg' alt="Google" width={70} height={70} ></Image>
             <h2 className='titulo capitalize font-bold text-lg '>Seo</h2>
             <h4 className='w-11/12 subtitulo text-center'>¡Eleva tu negocio con nuestro servicio de SEO para Google! Optimizamos tu sitio con palabras clave relevantes, mejorando tu posición en los resultados de búsqueda y atrayendo más clientes. ¡Contáctanos hoy!</h4>
@@ -93,25 +132,25 @@ export default function Home() {
 
         <div className='flex h-4/5 mt-10 lg:flex-row flex-col items-center gap-y-9 lg:gap-x-4 z-[2] relative px-5 sm:px-11 w-full'>
 
-          <div className='border-4 lg:pt-5  shadow-2xl relative h-[80%] rounded-sm borde-outset py-14 lg:p-3  flex justify-center flex-col items-center gap-y-4  '>
+          <div data-aos="flip-left" className='border-4 lg:pt-5  shadow-2xl relative h-[80%] rounded-sm borde-outset py-14 lg:p-3  flex justify-center flex-col items-center gap-y-4  '>
             <Image className='-translate-y-9 top-0 absolute' src='icon/angular.svg' alt="angular" width={70} height={70} ></Image>
             <h2 className='titulo capitalize text-lg font-bold' >AngularJS</h2>
             <h4 className='subtitulo lg:w-5/6 w-4/5 sm:w-4/6 leading-10 lg:text-base md:text-sm text-center'>Nuestro equipo de trabajo puede desarrollar desde páginas tipo catálogo, las cuales muestran sus productos de una forma sencilla y efectiva, hasta sitios web en varios idiomas, todo de acuerdo con sus necesidades.</h4>
           </div>
 
-          <div className='border-4 lg:pt-5  relative shadow-2xl h-[80%] rounded-sm borde-outset py-14 lg:p-3   flex justify-center flex-col items-center gap-y-4 '>
+          <div data-aos="flip-right" className='border-4 lg:pt-5  relative shadow-2xl h-[80%] rounded-sm borde-outset py-14 lg:p-3   flex justify-center flex-col items-center gap-y-4 '>
             <Image className='-translate-y-9 top-0 absolute' src='icon/react.svg' alt="react" width={70} height={70} ></Image>
             <h2 className='titulo capitalize text-lg font-bold'>ReactJS</h2>
             <h4 className='lg:w-5/6 w-4/5 sm:w-4/6 leading-10  subtitulo lg:text-base md:text-sm text-center'>¡Descubre el poder de un buen diseño web con nuestro servicio! En nuestro sitio, transformamos tus ideas en realidad digital. Nos especializamos en crear sitios web atractivos, intuitivos y de alto rendimiento que capturan la esencia de tu negocio y conectan con tus clientes.</h4>
           </div>
 
-          <div className='border-4 lg:pt-5  relative shadow-2xl h-[80%] rounded-sm borde-outset py-14 lg:p-3   flex justify-center flex-col items-center gap-y-4 '>
+          <div data-aos="flip-up" className='border-4 lg:pt-5  relative shadow-2xl h-[80%] rounded-sm borde-outset py-14 lg:p-3   flex justify-center flex-col items-center gap-y-4 '>
             <Image className='-translate-y-9 top-0 absolute' src='icon/next.svg' alt="next" width={70} height={70} ></Image>
             <h2 className='titulo capitalize text-lg font-bold'>NextJS</h2>
             <h4 className='lg:w-5/6 w-4/5 sm:w-4/6 leading-10  subtitulo lg:text-base md:text-sm text-center'>¡Eleva tu negocio con nuestro servicio de SEO para Google! Optimizamos tu sitio con palabras clave relevantes, mejorando tu posición en los resultados de búsqueda y atrayendo más clientes. ¡Contáctanos hoy!</h4>
           </div>
 
-          <div className='border-4 lg:pt-5  relative shadow-2xl h-[80%] rounded-sm borde-outset py-14 lg:p-3   flex justify-center flex-col items-center gap-y-4 '>
+          <div data-aos="flip-down" className='border-4 lg:pt-5  relative shadow-2xl h-[80%] rounded-sm borde-outset py-14 lg:p-3   flex justify-center flex-col items-center gap-y-4 '>
             <Image className='-translate-y-9 top-0 absolute' src='icon/wordpress.svg' alt="wordpress" width={70} height={70} ></Image>
             <h2 className='titulo capitalize text-lg font-bold'>WordPress</h2>
             <h4 className='lg:w-5/6 w-4/5 sm:w-4/6 leading-10  subtitulo lg:text-base md:text-sm text-center'>¡Eleva tu negocio con nuestro servicio de SEO para Google! Optimizamos tu sitio con palabras clave relevantes, mejorando tu posición en los resultados de búsqueda y atrayendo más clientes. ¡Contáctanos hoy!</h4>
@@ -153,10 +192,10 @@ export default function Home() {
 
             <div className='h-full top-0  w-full  absolute fondoColor-black'></div>
 
-            <div className="glass flex text-justify justify-center items-center">
+            <div   className="glass  flex text-justify justify-center items-center">
 
-              <div className="z-10 h-4/5 w-11/12  flex justify-center items-center">
-                <h3 className="subtitulo leading-loose text-white font-bold text-xs sm:text-base capitalize">
+              <div data-aos="zoom-in-up"  className="z-10 h-4/5 w-11/12  flex justify-center items-center">
+                <h3 className="subtitulo  leading-loose text-white font-bold text-xs sm:text-base capitalize">
                   <Typewriter
                     options={{
                       loop: false,
