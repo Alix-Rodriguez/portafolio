0f9e05812dbe02137eb31d6bb2632bc77ca7779e
Alix Rodriguez <alix.rodriguez@jirehpro.co>
2023-09-25 12:13:23 -0400

    modificaciones en la patanlla de inicio

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -10,7 +10,7 @@ import { Pagination, Navigation } from "swiper/modules";
 import Typewriter from 'typewriter-effect';
 import Image from "next/image";
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
 
@@ -48,31 +48,31 @@ export const Slider = () => {
 
     useEffect(() => {
         AOS.init({
-          // Global settings:
-          disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-          startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-          initClassName: 'aos-init', // class applied after initialization
-          animatedClassName: 'aos-animate', // class applied on animation
-          useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-          disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-          debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-          throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-          
-        
-          // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-          offset: 120, // offset (in px) from the original trigger point
-          delay: 100, // values from 0 to 3000, with step 50ms
-          duration: 1500, // values from 0 to 3000, with step 50ms
-          easing: 'ease', // default easing for AOS animations
-          once: false, // whether animation should happen only once - while scrolling down
-          mirror: true, // whether elements should animate out while scrolling past them
-          anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-        
+            // Global settings:
+            disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+            startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+            initClassName: 'aos-init', // class applied after initialization
+            animatedClassName: 'aos-animate', // class applied on animation
+            useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+            disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+            debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+            throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+            // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+            offset: 120, // offset (in px) from the original trigger point
+            delay: 100, // values from 0 to 3000, with step 50ms
+            duration: 1500, // values from 0 to 3000, with step 50ms
+            easing: 'ease', // default easing for AOS animations
+            once: false, // whether animation should happen only once - while scrolling down
+            mirror: true, // whether elements should animate out while scrolling past them
+            anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
         });
-    
-        
-        
-      }, []);
+
+
+
+    }, []);
     return (
         <>
             <Swiper
@@ -94,11 +94,12 @@ export const Slider = () => {
                             className="flex relative flex-col justify-center items-center leading-none h-screen w-screen bg-no-repeat bg-cover lg:bg-center "
                             style={sli.fondo}
                         >
-                            <div className="absolute h-screen w-full mySwipersli"></div>
+                            {/* <div className='h-full  w-full -rotate-180  absolute fondoColor-black'></div> */}
+
 
                             <div data-aos="flip-down" className="vidrio flex flex-col">
 
-                                <div  className="z-10 w-1/4 h-20 sm:h-44 sm:w-2/4 bg-cover absolute"
+                                <div className="z-10 w-1/4 h-20 sm:h-44 sm:w-2/4 bg-cover absolute"
                                     style={{ backgroundImage: "url('image/marco.png')" }} ></div>
 
                                 <div className="z-10 w-1/4 h-20 sm:h-44 sm:w-2/4 bg-cover absolute rotate-180 bottom-0 right-0"
