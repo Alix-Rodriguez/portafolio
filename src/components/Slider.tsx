776ad5c852bbcdcc79047a9ca3070188a91a1f16
Alix Rodriguez <alix.rodriguez@jirehpro.co>
2023-10-11 17:18:00 -0400

    cambiar imagenes a 100%

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -25,7 +25,7 @@ export const Slider = () => {
            
                         <div id="principal"
                             className="flex parallax relative flex-col justify-center items-center leading-none h-[65vh] object-cover lg:h-[100vh] w-screen "
-                            style={{backgroundImage: `url('image/start.png')`, backgroundSize: 'cover', backgroundPosition:'center center'}}
+                            style={{backgroundImage: `url('image/start.png')`, backgroundSize: '100% 100%', backgroundPosition:'center center'}}
                         >
                             
 
